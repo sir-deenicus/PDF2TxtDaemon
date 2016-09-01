4725bdcb27c223899a88ab1ac9fd5c06fa3cae61
Deen Abiola <sir.deenicus@gmail.com>
2016-09-01 02:47:53 +0100

    folders and annoying logs remove

--- src/PDFtxtDaemon.scala
+++ src/PDFtxtDaemon.scala
@@ -12,54 +12,32 @@ import org.apache.pdfbox.text.PDFTextStripper
 import scala.collection.JavaConversions._
 
 object PDFtxtDaemon {
+
+	System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
 	def main(args: Array[String]) {
 
 		val start = System.currentTimeMillis()
 		val count = args.length
-		var c = 1
+		var c = 0
 		val log = "pdf.log.txt"
+
 		val stripperg = new PDFTextStripper()
 
 		if (args.length > 0) {
-			args.par.foreach((filename) => {
+			args.foreach((filename) => {
+				val fname = new File(filename)
 
-				val pagemaker = new ObjectMapper()
-				val stripper = new PDFTextStripper()
-				val splitter = new Splitter()
-
-				val newpath = filename.replace(".pdf", ".txt")
-				println(c + "/" + count + " " + filename)
-				c += 1
-				println("  => " + newpath)
-				Files.write(Paths.get(log), (filename + ", " + ((System.currentTimeMillis() - start) / (1000.0 * 60.0))).getBytes)
-
-				val x0 = System.currentTimeMillis()
-				var doc = new PDDocument()
-
-				try {
-
-					doc = PDDocument.load(new File(filename))
-					splitter.setSplitAtPage(1)
-
-					val pages = new util.ArrayList(splitter.split(doc).map(p => stripper.getText(p)))
-
-					Files.write(Paths.get(newpath), pagemaker.writeValueAsString(pages).getBytes(StandardCharsets.UTF_8))
-
-					doc.close()
-
-					val now = System.currentTimeMillis()
-					val tot = (now - start) / (1000.0 * 60.0)
-					val pr = (now - x0) / 1000.0
-
-					println("Took " + pr + " seconds")
-					println("Total mins: " + tot)
-					println()
+				if (fname.isDirectory()) {
+					val files = fname.listFiles().filter(f => f.getName().toLowerCase().contains(".pdf"))
+					files.par.foreach(f => {
+						c+= 1
+						processfile(start, c, files.length, log, f, true)
+					})
 				}
-				catch {
-					case e: Throwable =>
-						Files.write(Paths.get(filename + ".error"), e.getMessage.getBytes(StandardCharsets.UTF_8))
-				} finally {
-					if (doc != null) doc.close()
+				else {
+					c += 1
+					processfile(start, c, count, log, fname, false)
 				}
 			})
 		}
@@ -74,7 +52,9 @@ object PDFtxtDaemon {
 							val pipe = new RandomAccessFile("\\\\.\\pipe\\pdfDaemon-commpipe", "rw")
 							pipebox = Some(pipe)
 						}
-						catch {case _ => ()}
+						catch {
+							case _ => ()
+						}
 					case Some(pipe) =>
 						val inp = pipe.readInt()
 
@@ -96,10 +76,64 @@ object PDFtxtDaemon {
 							}
 							catch {
 								case e: Throwable =>
-									println(e.getMessage())
+									val errmsg = "[ERROR]: " + e.getMessage()
+									val txtbytes = errmsg.getBytes(StandardCharsets.UTF_8)
+									val plen = ByteBuffer.allocate(4).putInt(txtbytes.length).array()
+									pipe.write(plen)
+									pipe.write(txtbytes)
+									println(errmsg)
 							}
 						}
 				}
+			}
+		}
+	}
+
+	def processfile(start: Long, c: Int, count: Int, log: String, filepath: File, isdir : Boolean): Unit = {
+
+		val newfileName = filepath.getName().replace(".pdf", ".txt")
+		if (!(new File(newfileName).exists())) {
+			val pagemaker = new ObjectMapper()
+			val stripper = new PDFTextStripper()
+			val splitter = new Splitter()
+
+			var doc = new PDDocument()
+
+			println(c + "/" + count + " " + filepath)
+
+			val x0 = System.currentTimeMillis()
+			try {
+
+				doc = PDDocument.load(filepath)
+				splitter.setSplitAtPage(1)
+
+				val pages = new util.ArrayList(splitter.split(doc).map(p => stripper.getText(p)))
+
+				val dir = filepath.getParent()
+
+				val fname = if(isdir) Paths.get(dir,"PDFs-Text",newfileName) else Paths.get(filepath.toString().replace(".pdf", ".txt"))
+
+				println("  => " + newfileName)
+
+				Files.write(fname, pagemaker.writeValueAsString(pages).getBytes(StandardCharsets.UTF_8))
+
+				Files.write(Paths.get(log), (filepath + ", " + ((System.currentTimeMillis() - start) / (1000.0 * 60.0))).getBytes)
+
+				val now = System.currentTimeMillis()
+				val tot = (now - start) / (1000.0 * 60.0)
+				val pr = (now - x0) / 1000.0
+
+				println("Took " + pr + " seconds")
+				println("Total mins: " + tot)
+				println()
+
+				doc.close()
+			}
+			catch {
+				case e: Throwable =>
+					Files.write(Paths.get(filepath + ".error"), e.getMessage.getBytes(StandardCharsets.UTF_8))
+			} finally {
+				if (doc != null) doc.close()
 			}
 		}
 	}
